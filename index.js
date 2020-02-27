6c163951b72a9ef67985859ff3969b0b56c87936
evergreen power <evergreenpower.development@gmail.com>
2020-02-27 16:05:35 +1100

    semis

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const test = 'tst';
 const test33 = 4565;
 const test44 = 4565;
 
-const test2 = 'new branch test222'
-const news = 898
+const test2 = 'new branch test222';
+const news = 898;
 // hula
 
 //hop
