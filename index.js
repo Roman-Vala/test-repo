9a31b029f480a77cca3ba0479703fa298b8da134
evergreen power <evergreenpower.development@gmail.com>
2020-02-27 15:42:44 +1100

    comm 567

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const test44 = 4565;
 
 const test2 = 'new branch test222'
 //continue to write code
- // git is reaaly stupidly designed <!DOCTYPE html>
+ // git is reaaly stupidly designed 
  
 // stupid comment
